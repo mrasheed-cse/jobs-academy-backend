c902b93edd689c8017a8e34c7d1fe19025bd7cb6
mrasheed-cse <your@email.com>
2026-08-26 14:26:14 +0600

    Fix insert-question URL indentation in urlpatterns

--- exam_import/urls.py
+++ exam_import/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import StartImportView, FixMathNotationView, ImportStatusView, PastExamListAdminView, PastExamDetailView, ExamQuestionsAdminView, QuestionEditView, OptionEditView, ExamPublishView
+from .views import StartImportView, InsertQuestionView, FixMathNotationView, ImportStatusView, PastExamListAdminView, PastExamDetailView, ExamQuestionsAdminView, QuestionEditView, OptionEditView, ExamPublishView
 
 urlpatterns = [
     path('api/exam-import/start/',              StartImportView.as_view(),       name='exam-import-start'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('api/exam-import/exams/<int:exam_id>/publish/', ExamPublishView.as_view(), name='exam-publish'),
     path('api/exam-import/questions/<int:question_id>/', QuestionEditView.as_view(), name='question-edit'),
     path('api/exam-import/options/<int:option_id>/', OptionEditView.as_view(), name='option-edit'),
+    path('api/exam-import/exams/<int:exam_id>/insert-question/', InsertQuestionView.as_view(), name='insert-question'),
 ]
